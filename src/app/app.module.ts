74e63fc0c4dfe6e1d3f25cf12f82a2fc70dc7847
Damian Maduzia <damian.maduzia@futurum.tech>
2020-09-20 17:40:46 +0200

    further restructuring, added typeahead input with test values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { FormsModule } from '@angular/forms';
+
 import {
   FontAwesomeModule,
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { UserInputComponent } from './components/user-input/user-input.component';
+import { UserInputComponent } from './components/main/user-input/user-input.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, UserInputComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    UserInputComponent,
+    MainComponent,
+  ],
   imports: [
+    FormsModule,
+    TypeaheadModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
